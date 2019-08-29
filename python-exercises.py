5a54ebe660d252cd3025276338f0d52bdbf221d1
Patrick Moore <patrickwmoore88@gmail.com>
2019-08-29 13:04:50 -0500

    All caps name

---
+++ python-exercises.py
@@ -0,0 +1,6 @@
+name= input('What is your name? ')
+#hello= "Hello, " + name
+name_length= len(name)
+print(f"Hello, {name}".upper())
+print(f"Your name has {name_length} letters in it! Awesome! ".upper())
+
