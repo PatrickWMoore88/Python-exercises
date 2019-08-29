bcde434e1c7c492ebc0c857f12525fd9f89f2a4d
Patrick Moore <patrickwmoore88@gmail.com>
2019-08-29 14:11:11 -0500

    Split Tip Calculator with improvements on the code for the floats and added $ for all the numbers

--- python-exercises.py
+++ python-exercises.py
@@ -1,16 +1,24 @@
-total_bill= int(input('Total bill amount? '))
-service_level= input('Level of service? ')
+total_bill = float(input("Total bill amount? $"))
+service_level = input("Level of service? ")
+split = int(input("Split how many ways? "))
 if service_level == "good" or service_level == "Good" or service_level == "GOOD":
-    good = total_bill * .15
-    print(f"Tip amount: {good}")
-    print(f"Total bill: {total_bill + good}")
+    good = total_bill * .2
+    print(f"Tip amount: ${good}")
+    total_plus_tax = total_bill + good
+    print(f"Total bill: ${total_plus_tax}")
+    total_per_person = round(total_plus_tax / split, 2)
+    print(f"Amount per person: ${total_per_person}")
 elif service_level == "fair" or service_level == "Fair" or service_level == "FAIR":
-    fair = total_bill * .12
-    print(f"Tip amount: {fair}")
-    print(f"Total bill: {total_bill + fair}")
+    fair = total_bill * .15
+    print(f"Tip amount: ${fair}")
+    total_plus_tax = total_bill + fair
+    print(f"Total bill: ${total_per_person}")
 elif service_level == "bad" or service_level == "Bad" or service_level == "BAD" or service_level == "BAD!":
     bad = total_bill * .10
-    print(f"Tip amount: {bad}")
-    print(f"Total bill: {total_bill + bad}")
+    print(f"Tip amount: ${bad}")
+    total_plus_tax = total_bill + bad
+    print(f"Total bill: ${total_plus_tax}")
+    total_per_person = round(total_plus_tax / split, 2)
+    print(f"Amount per person: ${total_per_person}")
 else: 
     print("Don't be cheap!")
