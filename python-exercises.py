b8e6230844c97597c6820d5202b17e203ce32c69
Patrick Moore <patrickwmoore88@gmail.com>
2019-08-29 13:52:58 -0500

    Tip calculator corrected with new print statements

--- python-exercises.py
+++ python-exercises.py
@@ -2,12 +2,15 @@ total_bill= int(input('Total bill amount? '))
 service_level= input('Level of service? ')
 if service_level == "good" or service_level == "Good" or service_level == "GOOD":
     good = total_bill * .15
-    print(total_bill + good)
+    print(f"Tip amount: {good}")
+    print(f"Total bill: {total_bill + good}")
 elif service_level == "fair" or service_level == "Fair" or service_level == "FAIR":
     fair = total_bill * .12
-    print(total_bill + fair)
+    print(f"Tip amount: {fair}")
+    print(f"Total bill: {total_bill + fair}")
 elif service_level == "bad" or service_level == "Bad" or service_level == "BAD" or service_level == "BAD!":
     bad = total_bill * .10
-    print(total_bill + bad)
+    print(f"Tip amount: {bad}")
+    print(f"Total bill: {total_bill + bad}")
 else: 
     print("Don't be cheap!")
