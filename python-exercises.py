0f53299e86fae0be8b0041a5752195d7709c03f0
Patrick Moore <patrickwmoore88@gmail.com>
2019-08-29 15:27:13 -0500

    Want a coin

--- python-exercises.py
+++ python-exercises.py
@@ -1,4 +1,15 @@
-to_ten = 0
-while to_ten < 10:
-    to_ten = to_ten + 1
-    print(to_ten)
+coins = 0
+coin_phrase = f"You have {coins} coins."
+print(coin_phrase)
+coin_question = input("Do you want another? ")
+while coin_question == "yes" or coin_question == "YES" or coin_question == "Yes" :
+    if coin_question == "yes" or coin_question == "YES" or coin_question == "Yes" :
+        coins = coins + 1
+        coin_phrase = f"You have {coins} coins."
+        print(coin_phrase)
+        coin_question= input("Do you want another? ")
+    else:
+        break
+print("Bye")
+
+
